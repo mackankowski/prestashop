4ff114462625ff67192d24d45125c04d2a1f985c
Maciej Kankowski <maciej.kankowski@jitsolutions.pl>
2018-11-07 20:14:01 +0100

    Fix domain url

--- fix.sh
+++ fix.sh
@@ -2,6 +2,6 @@
 domain=$1
 if [ -z $domain ]
 then
-  domain="localhost"
+  domain="localhost:8888"
 fi
 sudo docker exec -it mysql mysql -p -e "use prestashop; update ps_shop_url set domain='$domain', domain_ssl='$domain' where id_shop_url = 1; select * from ps_shop_url"
